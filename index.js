51acbf2a4fcb1453f33b40b7b4a0ee7f322dee78
Stefan Stefanov <s.hr.stefanov@gmail.com>
2015-10-03 16:54:14 +0300

    wip

---
+++ index.js
@@ -0,0 +1,20 @@
+var i18n   = require("i18next-client");
+
+module.exports = require("lib.Controller").extend("I18nextController", {
+
+  data: {
+    t: function(s){return s;},
+    i18n: i18n
+  },
+
+
+  init: function(options, cb){
+    var self = this;
+    i18n.init(this.config || {}, function(err, t){
+      if(err) return cb(err);
+      require("app").set("t", t);
+      cb();
+      self.trigger("init");
+    });
+  }
+});
